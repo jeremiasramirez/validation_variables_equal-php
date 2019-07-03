7e63edb12f3df99d55c5461ed2bde997534cccf1
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-03 17:53:32 -0400

    verificando que los valores no sean vacios

--- Validation.php
+++ Validation.php
@@ -2,18 +2,21 @@
 
 
 function Validation($num, $num2){
+	if(empty($num) and empty($num2)){
 
-	if($num == $num2 and $num === $num2){
-		 // tienen el mismo valor y son del mismo tipo
-		return 1;
-	}
-	else if($num == $num2){
-		 // tienen el mismo valor pero no son del mismo tipo
-		return 0;
-	}
-	else{
-		 // No tienen el mismo valor tampoco son del mismo tipo
-		return -1;	
+
+		if($num == $num2 and $num === $num2){
+			 // tienen el mismo valor y son del mismo tipo
+			return 1;
+		}
+		else if($num == $num2){
+			 // tienen el mismo valor pero no son del mismo tipo
+			return 0;
+		}
+		else{
+			 // No tienen el mismo valor tampoco son del mismo tipo
+			return -1;	
+		}
 	}
 
 }
